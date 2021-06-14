dcc3a41475d80a021a204461c51ee77ca15af459
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-06-14 20:56:26 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    persons: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data);
+      const persons = res.data;
+      this.setState({ persons });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          {this.state.persons.map((person) => (
+            <li>{person.name}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
-
 export default App;
